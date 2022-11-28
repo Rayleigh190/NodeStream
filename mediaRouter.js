1df432f49f31a0c3ab779b73d8237cb8e071600d
rayleigh <rayleigh190@gmail.com>
2022-11-29 00:43:36 +0900

    modify: 미디어 라우터 구분

--- mediaRouter.js
+++ mediaRouter.js
@@ -3,28 +3,32 @@ const multiparty = require("multiparty");
 const url = require("url");
 const fs = require("fs");
 
-router.get("/*", (req, res) => {
+router.get("/audio/*", (req, res) => {
   const { pathname } = url.parse(req.url, true);
   const filepath = `./resource${pathname}`;
 
   const stat = fs.statSync(filepath);
   const fileSize = stat.size;
   const range = req.headers.range;
+  // console.log("stat: " + stat);
+  // console.log("fileSize: " + fileSize);
   console.log("range: " + range);
 
   if (!range) {
+    // console.log("no");
     const header = { "Content-Type": "audio/mpeg" };
     res.writeHead(200, header);
     res.end();
   } else {
-    const MAX_CHUNK_SIZE = 1000 * 1000 * 2;
+    // console.log("yes");
+    const MAX_CHUNK_SIZE = 1000 * 1000;
     // ranage헤더 파싱
     const parts = range.replace(/bytes=/, "").split("-");
     // 재생 구간 설정
     const start = parseInt(parts[0], 10);
     const _end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
     const end = Math.min(_end, start + MAX_CHUNK_SIZE - 1);
-
+    // console.log("end: " + end);
     const header = {
       "Content-Range": `bytes ${start}-${end}/${fileSize}`,
       "Accept-Ranges": "bytes",
@@ -32,7 +36,48 @@ router.get("/*", (req, res) => {
       "Content-Length": fileSize - 1,
     };
     res.writeHead(206, header);
+    // console.log("res" + res);
     const readStream = fs.createReadStream(filepath, { start, end });
+    // console.log("readStream" + readStream);
+    readStream.pipe(res);
+  }
+});
+router.get("/video/*", (req, res) => {
+  const { pathname } = url.parse(req.url, true);
+  const filepath = `./resource${pathname}`;
+
+  const stat = fs.statSync(filepath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+  // console.log("stat: " + stat);
+  // console.log("fileSize: " + fileSize);
+  console.log("range: " + range);
+
+  if (!range) {
+    // console.log("no");
+    const header = { "Content-Type": "video/mp4" };
+    res.writeHead(200, header);
+    res.end();
+  } else {
+    // console.log("yes");
+    const MAX_CHUNK_SIZE = 1000 * 1000 * 50;
+    // ranage헤더 파싱
+    const parts = range.replace(/bytes=/, "").split("-");
+    // 재생 구간 설정
+    const start = parseInt(parts[0], 10);
+    const _end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const end = Math.min(_end, start + MAX_CHUNK_SIZE - 1);
+    // console.log("end: " + end);
+    const header = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Type": "video/mp4",
+      "Content-Length": fileSize - 1,
+    };
+    res.writeHead(206, header);
+    // console.log("res" + res);
+    const readStream = fs.createReadStream(filepath, { start, end });
+    // console.log("readStream" + readStream);
     readStream.pipe(res);
   }
 });
